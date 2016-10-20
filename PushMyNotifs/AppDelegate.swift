8680dce296df82c6021c0c7f4e6db3a8ec838aec
Patrick Pahl <ppahl84@yahoo.com>
2016-10-19 23:05:21 -0600

    InitialCommit

--- PushMyNotifs/AppDelegate.swift
+++ PushMyNotifs/AppDelegate.swift
@@ -4,9 +4,42 @@
 //
 //  Created by Patrick Pahl on 10/19/16.
 //  Copyright © 2016 Patrick Pahl. All rights reserved.
+
+///For push notifcations: developer.apple.com
+//account -> certificates
+//Create app id: Click App Id (left)
+//copy bundle identifier
+//Enter name: i.e. 'PushMyNotifs', and paste bundleID
+//***Click Push Notifications
+//Continue/Register/Done
+//development (left side), click + (top right)
+//Apple push notification service SSL
+//select project i.e. PushMyNotifs
+//Create CSR file: Go to keychain access app
+//KeyChain Access -> Certificate Assistant -> Req Cert from a Cert Authority
+//Enter email/name AND Save to Disk AND specify key pair information
+//Continue/then quit keychain
+//back to apple.developer.com -> Pick the file you put on your desktop
+//Then download it, click it to install it in keychain access
+//Click OK to add it
+//go to FIREBASE, click gear by top-left of project, project settings
+//Click Cloud Messaging
+//Under development, unpload certificate (Not the one you just DL'ed, p12 file)
+//Go to Keychain, find file i.e. PushMyNotifs (On left: login top, certs below)
+//expand, right-click the key, export.
+//Save as certificates, p12 file.
+//Create password for the file
+//Back to Firebase, upload cert to development
+//browse for p12 file and enter password
+//in app, register for push notifications and set entitlements
+//Xcode: top left project file, capabilites, click push notifications, turn ON
 //
 
+
+
 import UIKit
+import Firebase
+///Added!
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +48,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FIRApp.configure()
+        ///ADDED!
+        
         return true
     }
 
